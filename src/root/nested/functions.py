b0589aba9643c9a606644cef822b08edb70e10d9
magic-muschrum <worldwidetobi@gmx.de>
2018-09-20 00:41:12 +0200

    updating add student function and saving file

--- src/root/nested/functions.py
+++ src/root/nested/functions.py
@@ -4,7 +4,6 @@ Created on 29.05.2018
 @author: tpilz
 '''
 from test.test_decimal import file
-import csv
 
 students = []
 
@@ -12,7 +11,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title();
+        students_titlecase.append(student["name"].title());
     return students_titlecase
 
 def print_students_titlecase():
@@ -45,14 +44,15 @@ def enter_students():
         student_name = input("Enter student name: ")
         student_id = input("Enter student ID: ")
         add_student(student_name, student_id)
+        save_file(student_name)
     
     for student in students:
-        print("Student number {0} is {1} and his ID is {2}".format(students.index(student)+1, student["name"].title(), student["student_id"]))                
+        print("Student number {0} is {1} and his ID is {2}".format(students.index(student)+1, student["name"].title(), student["student_id"]))              
             
 read_file()
 print_students_titlecase()
 enter_students()
-save_file(students)
+
 
 #student_list = get_students_titlecase() 
 #add_student(student_name, student_id)
